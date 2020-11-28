521bb516f981166b06df3b9ca0fcdc810d6b76b8
kanatti <balukanattil@gmail.com>
2020-11-29 00:41:55 +0530

    Add basic GET/SET implementation for mini-redis

---
+++ my-redis/examples/hello-redis.rs
@@ -0,0 +1,16 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    // Open a connection to the mini-redis address
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // Set the key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello").await?;
+
+    println!("Got the value from server: {:?}", result);
+
+    Ok(())
+}
---
+++ my-redis/src/main.rs
@@ -0,0 +1,47 @@
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+
+#[tokio::main]
+async fn main() {
+    // Bind the listener to the address
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let (socket, addr) = listener.accept().await.unwrap();
+        println!("Accepted new connection {:?}", addr);
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    // A hashmap is used to store data;
+    let mut db = HashMap::new();
+
+    // The Connection lets us read/write redis frames instead of 
+    // byte streams.
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        connection.write_frame(&response).await.unwrap();
+    }
+}
